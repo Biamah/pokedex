3fd088de9dd76415a822a62a047806a8762fbe22
dbasilioesp <dbasilioesp@gmail.com>
2022-10-20 09:14:55 -0300

    feat: ajusta configs de routerdom em indexjs para abrir app em path / em andamento

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Outlet } from 'react-router-dom';
+import logo from '../src/assets/logo.png';
 
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <a href='/'>
+          <img src={logo} alt='Pokebola em preto e branco' />
+          <h1>Pokémon</h1>
+        </a>
+      </div>
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 }
